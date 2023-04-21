74c9d62f9b19581094d36debcaeb8ea594ff2a53
ErminaDelPilar <130517785+ErminaDelPilar@users.noreply.github.com>
2023-04-21 13:29:13 -0700

    Update First.java

--- src/main/java/edu/co/First.java
+++ src/main/java/edu/co/First.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class First {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "F:\\Selenium\\chromedriver_win32\\chromedriver.exe");  
+		System.setProperty("webdriver.chrome.driver", "C:\Users\ermina\eclipse-workspace\Drivers\\chromedriver.exe");  
 		WebDriver driver = new ChromeDriver();  
 	      
 	// Launch website  
